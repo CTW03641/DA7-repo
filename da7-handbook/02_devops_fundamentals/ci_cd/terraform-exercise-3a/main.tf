5d8b84b3a73a11a34595d62516f3bc7efed71247
CTW03641 <ctw03641@criticaltechworks.com>
2024-04-22 15:10:53 +0100

    Fixed CKV_AWS_355

--- da7-handbook/02_devops_fundamentals/ci_cd/terraform-exercise-3a/main.tf
+++ da7-handbook/02_devops_fundamentals/ci_cd/terraform-exercise-3a/main.tf
@@ -9,17 +9,35 @@ resource "aws_s3_bucket" "your_bucket" {
   bucket = "${var.project_name}-${var.identifier}"
 }
 
+# resource "aws_iam_policy" "lambda_policy" {
+#   name   = "${local.prefix}_lambda_module_policy"
+#   policy = jsonencode({
+#     Version   = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "s3:*",
+#         ]
+#         Resource = ["*"]
+#       }
+#     ]
+#   })
+# }
+
 resource "aws_iam_policy" "lambda_policy" {
   name   = "${local.prefix}_lambda_module_policy"
   policy = jsonencode({
     Version   = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
-        Action = [
-          "s3:*",
+        Effect   = "Allow"
+        Action   = [
+          "s3:GetObject",     // Allows reading objects from S3 buckets
+          "s3:PutObject",     // Allows writing objects to S3 buckets
+          // Add additional actions for write operations if needed
         ]
-        Resource = ["*"]
+        Resource = ["*"]      // Allows access to all resources in S3 buckets
       }
     ]
   })
